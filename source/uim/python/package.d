c45680f87fc67530cbf3647dbd3d505b35895316
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-05-18 14:23:37 +0200

    Updates

--- source/uim/python/package.d
+++ source/uim/python/package.d
@@ -1,2 +1,14 @@
 module uim.python;
 
+public import std.algorithm;
+public import std.array;
+public import std.conv;
+public import std.range;
+public import std.stdio;
+public import std.string;
+public import std.uuid;
+
+public import vibe.d;
+
+public import uim.core;
+public import uim.oop;
--- source/uim/python/parser/package.d
+++ source/uim/python/parser/package.d
@@ -1,3 +1,21 @@
 module uim.python.parser;
 
 import uim.python;
+
+auto parse(string content) {
+  auto isComment = (string line) => line.startsWith('#');
+  auto isSignificant = (string line) => !line.empty && !isComment(line);
+
+  return content
+  .splitter('\n')
+  .map!strip
+  .filter!isSignificant
+  .array;
+}
+unittest {
+  writeln("XXX");
+  writeln(parse(`var1 = "Die Antwort ist"
+print(var1)
+var1 = 42
+print(var1)`));
+}
